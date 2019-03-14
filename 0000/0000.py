57d9c6122a13495b678b9ad925f1292926194997
fynn-mac <995331775@qq.com>
2019-03-14 23:16:12 +0800

    commit

---
+++ 0000/0000.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# _*_coding:utf-8 _*_
+"""
+-------------------------------------------------
+   File Name:     0000.py
+   Author:        maple
+   date:          2019-03-13 18:03
+   Software:      pycharm
+-------------------------------------------------
+"""
+
+'''
+第 0000 题：
+将你的 QQ 头像（或者微博头像）右上角加上红色的数字，类似于微信未读信息数量那种提示效果。
+'''
+
+'''
+思路：
+PIL中Image类可以读取图片，并对图片内容进行修改。
+'''
+
+###########
+'''
+优化：
+1、比如对任意一张图片进行图片规整，之后对任意长度对数据均能正常显示在图片上
+2、可以判断图片对色彩，自动选择匹配对文字颜色
+'''
+
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+
+img = Image.open("down.jpeg")
+draw = ImageDraw.Draw(img)
+fontStyle = ImageFont.truetype("Arial.ttf",150,encoding="unic")
+width,height = img.size
+draw.text((width/1.3,20),"100",fill=(255,0,0),font=fontStyle)
+img.save("new.jpg")
---
+++ 0001/0001.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+   File Name:     0001.py
+   Author:        maple
+   date:          2019/3/13 21:26
+   Software:      pycharm
+-------------------------------------------------
+"""
+
+'''
+第 0001 题：
+做为 Apple Store App 独立开发者，你要搞限时促销，为你的应用生成激活码（或者优惠券），
+使用 Python 如何生成 200 个激活码（或者优惠券）？
+'''
+
+'''
+思路：
+1、产生随机大写字母函数
+2、产生随机小写字母函数
+3、产生随机数字函数
+4、随机选择其中一种方式，连续随机codeLenth次
+5、判断与之前产生是否重复，去重处理
+6、重复，直至产生codeNum个激活码
+'''
+
+'''
+优化：
+
+'''
+
+import random
+
+def getStringRandom(codeLenth):
+    code = ""
+    for i in range(0,codeLenth):
+        tmp = random.randint(1,3)
+        if tmp == '1':
+            code = code + str(getRandomNum())
+        elif tmp == '2':
+            code = code + chr(getUpChar())
+        else:
+            code = code + chr(getLowerChar())
+
+    return code
+
+#随机数字
+def getRandomNum():
+    return random.randint(0,9)
+
+#随机大写ASCII码
+def getUpChar():
+    return random.randint(0,25)+97
+
+#随机小写ASCII码
+def getLowerChar():
+    return random.randint(0,25)+97
+
+if __name__ == '__main__':
+    codeNum = eval(input("输入需要获取的激活码的数量:\n"))
+    codeLenth = eval(input("输入激活码的长度:\n"))
+    i = 0
+    codeList = []
+    while i < codeNum :
+        tmp = getStringRandom(codeLenth)
+        if tmp not in codeList:
+            codeList.append(tmp)
+            i = i+1
+    for i in codeList:
+        print (i)
