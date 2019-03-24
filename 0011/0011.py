a80048121a4371d8a84f57ce3d513b3ed681cb96
fynn-mac <995331775@qq.com>
2019-03-24 19:41:09 +0800

    0011框架设计

---
+++ 0011/0011.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+   File Name:     0011.py
+   Author:        Fynn-mac
+   date:          2019-03-24 17:35
+   Software:      PyCharm
+-------------------------------------------------
+"""
+
+'''
+第0011题：
+敏感词文本文件 filtered_words.txt，里面的内容为以下内容，当用户输入敏感词语时，则打印出 Freedom，
+否则打印出 Human Rights
+'''
+
+'''
+思路：
+1、先读取文本对单词，形成list列表
+2、对用户输入对内容进行判断，如包含在内，就进行剔除处理
+'''
+
+'''
+优化：
+
+'''
+def checkWord(str):
+    list = readTxt()
+    newStr= str
+    replaceStr = str
+    for i in list:
+        if i in str:
+            newStr = "Freedom"
+            print(i)
+            break
+        else:
+            newStr = "Human Rights"
+    print(newStr)
+
+def readTxt():
+    list = ["北京","程序员","公务员","领导","牛比","牛逼","你娘"]
+    with open("filtered_words.txt","wb") as f:
+        txt = f.read()
+        print(txt)
+    return list
+
+if __name__ == '__main__':
+    str = input("请输入内容：")
+    checkWord(str)
