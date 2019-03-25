b7d3ab4fcaa7947ce56bef639327b8ceee0387f0
fynn-pc <995331775@qq.com>
2019-03-25 23:36:20 +0800

    提交0012

--- 0012/0012.py
+++ 0012/0012.py
@@ -11,12 +11,14 @@
 
 '''
 第0012题：
-
+敏感词文本文件 filtered_words.txt，里面的内容 和 0011题一样，当用户输入敏感词语，则用 星号 * 替换，例如当用户输入「北京是个好城市」，则变成「**是个好城市」。
 '''
 
 '''
 思路：
-
+1、读文件
+2、对输入信息进行替换
+3、replace函数的用法，是返回一个新的字符串，并不是直接对字符串进行操作。
 '''
 
 '''
@@ -26,26 +28,19 @@
 
 def checkWord(str):
     list = readTxt()
-    newStr= str
     replaceStr = str
     for i in list:
         if i in str:
-            newStr = "Freedom"
-            print(i)
-            replaceStr.replace("北京","**")
-            break
-        else:
-            newStr = "Human Rights"
-    print(newStr)
+            replaceStr = replaceStr.replace(i,"***")
     print(replaceStr)
 
 def readTxt():
     words = []
-    with open("filtered_words.txt","r") as f:
+    with open("filtered_words.txt",'r',encoding='UTF-8') as f:
         for word in f.readlines():
             words.append(word.strip())
     return words
 
 if __name__ == '__main__':
-    str = input("请输入内容：")
+    str = input("请输入内容：\n")
     checkWord(str)
