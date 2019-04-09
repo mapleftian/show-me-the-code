0ca866a6998477f61dfbe123fa1df2fdd55907b4
fynn <995331775@qq.com>
2019-04-09 23:26:53 +0800

    提交0017

--- 0017/0017.py
+++ 0017/0017.py
@@ -14,39 +14,57 @@
 将 第 0014 题中的 student.xls 文件中的内容写到 student.xml 文件中
 '''
 
-'''
-思路：
-读取excel中的文本信息
-使用json.dump方式进行写入
-'''
-
-'''
-优化：
-使用dumps写入
-'''
-
-import json
 import xlrd
-
+from xml.etree.ElementTree import Element, SubElement, Comment, ElementTree
+import xml.dom.minidom as md
 
 def openXls():
     # 读取excel，读取sheet
     data = xlrd.open_workbook("student.xls")
     student = data.sheet_by_name("student")
     dict = {}
-    list = []
     nor = student.nrows
-    nol = student.ncols
-    for x in range(0, nor):
-        title = student.cell(x, 0)
-        for y in range(1, nol):
-            value = student.cell(x, y)
-            list.append(value)
-        dict[title] = list
-        list = []
+    for x in range(nor):
+        dict[x + 1] = student.row_values(x)[1:]
     return dict
 
 
+def write_to_xml(xlscontent):
+    # 创建新xml文件
+    xmlfile = md.Document()
+
+    # 创建节点
+    root = xmlfile.createElement('root')
+    # 创建节点
+    students = xmlfile.createElement('students')
+    # 在文件中添加root节点
+    xmlfile.appendChild(root)
+    # 在root下添加students节点
+    root.appendChild(students)
+    # 创建评论
+    comment = xmlfile.createComment('学生信息表 "id" : [名字, 数学, 语文, 英文]')
+    # 在students标签下添加comment
+    students.appendChild(comment)
+    # 创建文本节点
+    xmlcontent = xmlfile.createTextNode(str(xlscontent))
+    # 在students标签下添加文本内容
+    students.appendChild(xmlcontent)
+    # 写入文件
+    with open('students.xml', 'wb') as f:
+        f.write(xmlfile.toprettyxml(encoding='utf-8'))
+
+
+def write_to_xml_2(data):
+    root = Element('root')
+    comment = Comment('学生信息表"id" : [名字, 数学, 语文, 英文]')
+    root.append(comment)
+    child = SubElement(root, 'students')
+    child.text = str(data)
+    tree = ElementTree(root)
+    tree.write('student2.xml', encoding='utf8')
+
 if __name__ == '__main__':
-    dict = openXls()
-    print(dict)
+    data = openXls()
+    print(data)
+    write_to_xml(data)
+    write_to_xml_2(data)
